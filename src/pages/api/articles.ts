179f8d6c2ae79905942d2d1862403e12764fee21
FireBEAR12138 <805626176@qq.com>
2024-11-05 16:35:19 +0800

    添加了正确的 Next.js 配置

--- src/pages/api/articles.ts
+++ src/pages/api/articles.ts
@@ -6,10 +6,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
   try {
     const {
       page = 1,
