77e91054c0a39b1e3bfdef7ff9d4d5ff63c19e39
FireBEAR12138 <805626176@qq.com>
2024-11-08 09:59:48 +0800

    导入 SourceModal 组件

--- src/components/RSSManager.tsx
+++ src/components/RSSManager.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { FaPlay, FaEdit, FaTrash, FaPlus } from 'react-icons/fa';
+import SourceModal from './SourceModal';
 
 interface RSSSource {
   id: number;
